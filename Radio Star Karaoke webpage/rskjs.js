4059167c4db45c8e0dee5222b85abd3a0141d35d
Ber1itz <97990880+Ber1itz@users.noreply.github.com>
2022-10-30 19:34:41 -0400

    Add files via upload

--- Radio Star Karaoke webpage/rskjs.js
+++ Radio Star Karaoke webpage/rskjs.js
@@ -1,16 +1,14 @@
 String.prototype.replaceAll = function(strTarget, strSubString){
   var strText = this;
   var intIndexOfMatch = strText.indexOf( strTarget );
-  // Keep looping while an instance of the target string
-  // still exists in the string.
+
   while (intIndexOfMatch != -1){
-  // Relace out the current instance.
+
   strText = strText.replace( strTarget, strSubString )
-  // Get the index of any next matching substring.
+
   intIndexOfMatch = strText.indexOf( strTarget );
   }
-  // Return the updated string with ALL the target strings
-  // replaced out with the new substring.
+
   return( strText );
 }
 
@@ -87,11 +85,11 @@ function formatCurrentTime(time) {
   if (ms < 10) { // 0
     ms = "0" + ms;
   }
-  if (10 <= ms < 100) { //00
+  if (10 <= ms < 100) {
     var convMsToString = ms.toString().slice(0);
     ms = "0" + convMsToString;
   }
-  if (100 <= ms < 1000) { //000
+  if (100 <= ms < 1000) {
     var convMsToString = ms.toString().slice(-3, -1);
     ms = convMsToString;
   }
@@ -102,8 +100,7 @@ function timeToInt(time) {
   var timeMinute = time.slice(1, 3);
   var timeSecond = time.slice(4, 6);
   var timeMs = time.slice(7, 9);
-  // console.log("TimeToIntImport", time);
-  // console.log("TimeToInt", timeMinute + ":" + timeSecond + ":" + timeMs);
+
   var result = parseInt(timeMinute)+parseInt(timeSecond)+parseInt(timeMs);
   return result;
 }
@@ -131,7 +128,6 @@ function loadFileAsText(){
     addStyleLyric = addStyleLyric.replaceAll("</span><br />,<span", "</span><br /><span");
     document.getElementById("inputTextToSave").innerHTML = addStyleLyric;
 
-    //Run Lyric
     runLyric(textFromFileLoaded);
 
   };
@@ -147,80 +143,65 @@ function visualization() {
   canvasElm.width = window.innerWidth;
   canvasElm.height = window.innerHeight;
   var audioContext = new AudioContext();
-    // Khởi tạo AudioContext source
+
     var audioContextSrc = audioContext.createMediaElementSource(audioSong);
-    // Khởi tạo Analyser
+
     var audioAnalyser = audioContext.createAnalyser();
-    // Khởi tạo 2D canvas
+
     canvasContext = canvasElm.getContext("2d");
 
-    // Kết nối AudioContext source với Analyser
     audioContextSrc.connect(audioAnalyser);
-    // Kết nối Analyser với AudioDestinationNode
+
     audioAnalyser.connect(audioContext.destination);
 
-    // Gán FFT size là 256 cho Analyser
-    // Các bạn có thể xem thêm tại: https://developer.mozilla.org/en-US/docs/Web/API/AnalyserNode/fftSize
     audioAnalyser.fftSize = 1024;
 
-    // Lấy dữ liệu tần số từ Analyser
-    // https://developer.mozilla.org/en-US/docs/Web/API/AnalyserNode/frequencyBinCount
     var analyserFrequencyLength = audioAnalyser.frequencyBinCount;
-    // var analyserFrequencyLength = 256;
-    // var analyserFrequencyLength = 512;
-    // Khởi tạo một mảng số nguyên dương 8-bit có số lượng phần tử bằng analyserFrequencyLength
+
     var frequencyDataArray = new Uint8Array(analyserFrequencyLength);
-    // Lấy width và height của canvas
+
     var canvasWith = canvasElm.width;
     var canvasHeight = canvasElm.height;
 
-    // Tính toán barWidth và barHeight
+
     var barWidth = (canvasWith / analyserFrequencyLength) * 1.1;
     var barHeight;
     var barIndex = 0;
 
     function renderFrame() {
-      // Thông báo với trình duyệt rằng chúng ta đang chuẩn bị thực hiện một animation với method là như này. Hãy chuẩn bị đi =)
-      // https://developer.mozilla.org/en-US/docs/Web/API/window/requestAnimationFrame
+
       window.requestAnimationFrame(renderFrame);
 
-      // Reset lại barIndex trở về 0
       barIndex = 0;
 
-      // Điền dữ liệu tần số vào mảng
       audioAnalyser.getByteFrequencyData(frequencyDataArray);
 
-      // Vẽ một hình chữ nhật với nền màu đen
       canvasContext.fillStyle = "#000";
       canvasContext.fillRect(0, 0, canvasWith, canvasHeight);
 
-      // Chạy lần lượt từ 0 đến hết dữ liệu tần số của Analyser
       for (var i = 0; i < analyserFrequencyLength; i++) {
         barHeight = frequencyDataArray[i];
-        // Tạo màu cho thanh bar
+
         var rgbRed = barHeight + (25 * (i / analyserFrequencyLength));
         var rgbGreen = 250 * (i / analyserFrequencyLength);
         var rgbBlue = 50;
 
-        // Điền màu và vẽ bar
         canvasContext.fillStyle = "rgb("+ rgbRed +", "+ rgbGreen +", "+ rgbBlue +")";
         canvasContext.fillRect(barIndex, (canvasHeight - barHeight - 200), barWidth, (barHeight + 200));
         barIndex += (barWidth + 1);
       }
     }
-    // Gọi method để render vào canvas
+
     renderFrame();
 }
 
 function runLyric(str) {
-  // clearInterval(intervalObj);
   var myAudio = document.getElementById("audioSong");
   var playSongButton = document.querySelectorAll(".activeStartPause");
   var fileSong = document.getElementById("fileSong");
   var fileLyric = document.getElementById("fileLyric");
   var actStop = document.querySelectorAll(".activeStop");
 
-  // debugger;
   for (let i = 0; i < playSongButton.length; i++) {
     playSongButton[i].addEventListener("click", function(){
       clearInterval(intervalObj);
@@ -322,7 +303,6 @@ function runLyric(str) {
           if (lineLyricPass) {
             lineLyricPass.style.color = "#8d6e63";
           }
-          // window.scrollBy(0, 10);
           break;
       };
       linesongidBE++;
